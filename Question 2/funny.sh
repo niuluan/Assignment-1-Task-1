0038297333b19a3f6f36cac52ebd91dcb061fb80
niuluan <68548642+niuluan@users.noreply.github.com>
2021-03-18 18:22:28 +0800

    Add files via upload

---
+++ Question 2/funny.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# If an argument then if true 
+if [[ "$#" = 1 ]]
+then
+	echo "This is funny"
+
+# If no argument then if flase 
+elif [[ "$#" = 0 ]]
+then
+	echo "This is NOT funny"
+
+
+
+fi
+
+
